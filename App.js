48504ad53e44968b41df896efcf23d09efe8b893
Tadg Keating Webb <tadgkeating@gmail.com>
2022-09-21 14:06:01 +0700

    Updated import path for members dashboard

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from "./screens/Login";
-import Dashboard from "./screens/Dashboard";
+import Dashboard from "./screens/MemberView/Dashboard";
 import Groups from "./screens/Groups";
 import Group from "./screens/GroupView/Group";
 

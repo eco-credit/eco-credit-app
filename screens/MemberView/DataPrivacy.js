40f3ac7a8a14da1ab8216d913733060871b6640e
Tadg Keating Webb <tadgkeating@gmail.com>
2022-11-01 16:36:52 +0700

    Design: Data privacy page

--- screens/MemberView/DataPrivacy.js
+++ screens/MemberView/DataPrivacy.js
@@ -1,4 +1,5 @@
-import {Text, View} from "react-native";
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from "react-native";
+import CardComponent from "../../components/CardComponent";
 
 const DataPrivacy = ({ route }) => {
     const { member } = route.params
@@ -8,17 +9,22 @@ const DataPrivacy = ({ route }) => {
     }
 
     return (
-        <View>
-            <Text>
-                Data Processing:
-                { outputPrivacyOption(member.data_processing) }
-            </Text>
-            <Text>
-                Data Sharing:
-                { outputPrivacyOption(member.data_sharing) }
-            </Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                { CardComponent('Data Processing:', outputPrivacyOption(member.data_processing)) }
+                { CardComponent('Data Sharing:', outputPrivacyOption(member.data_sharing)) }
+            </ScrollView>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        paddingTop: 20,
+        backgroundColor: "#f5f6fa",
+    },
+})
 
 export default DataPrivacy

59b7f182f7dec73069141146544e00f120609580
ibrahim-arif <ibrahimrana232@gmail.com>
2023-01-18 15:30:55 +0500

    font and group screen crash issue resolved

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { MenuProvider } from "react-native-popup-menu";
+
 import Login from "./screens/Login";
 import Dashboard from "./screens/MemberView/Dashboard";
 import Groups from "./screens/Groups";
 import Group from "./screens/GroupView/Group";
-import { useEffect, useState } from "react";
 import LoadingComponent from "./components/LoadingComponent";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { MenuProvider } from "react-native-popup-menu";
 import Milestone from "./screens/MemberView/Milestone";
 
 const Stack = createNativeStackNavigator();
@@ -16,9 +17,9 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   const [defaultRoute, setDefaultRoute] = useState(null);
 
-  const getToken = async () => {
-    return await AsyncStorage.getItem("token");
-  };
+  const [fontsLoaded] = useFonts({
+    Roboto: require("./fonts/Roboto/Roboto-Regular.ttf"),
+  });
 
   useEffect(() => {
     getToken.call().then((token) => {
@@ -27,9 +28,9 @@ export default function App() {
     });
   }, []);
 
-  if (!defaultRoute) {
-    return <LoadingComponent />;
-  }
+  const getToken = async () => await AsyncStorage.getItem("token");
+
+  if (!defaultRoute || !fontsLoaded) return <LoadingComponent />;
 
   return (
     <MenuProvider>
